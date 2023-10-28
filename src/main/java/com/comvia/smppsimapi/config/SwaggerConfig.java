3edf10cd81f3b0fdde215eaf9047e7bb89071a43
Anket Pratap Singh <singhanket5101@gmail.com>
2023-10-28 18:36:42 +0530

    Sonar issue fixed

--- src/main/java/com/comvia/smppsimapi/config/SwaggerConfig.java
+++ src/main/java/com/comvia/smppsimapi/config/SwaggerConfig.java
@@ -17,7 +17,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 public class SwaggerConfig {
 
     @Bean
-    public Docket api() {
+    Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.comvia.smppsimapi"))
